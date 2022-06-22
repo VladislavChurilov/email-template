c51f599acb5a5ee78b6f6e77f16461df834b1ec7
Vladislav Churilov <88vvch88@gmail.com>
2022-06-22 09:53:55 +0300

    release

---
+++ index.js
@@ -0,0 +1,84 @@
+const fs = require('fs')
+const utils = require('util')
+const writeFile = utils.promisify(fs.writeFile)
+const path = require('path')
+const mjml2html = require('mjml')
+const Handlebars = require("handlebars")
+
+const successOrder = {
+    orderId: '1541234',
+    status: 'NEW',
+    createAt: '12.04.2021',
+    paySum: 300,
+    totalSum: 1200,
+    name: 'Константин Константинович Константинопольский',
+    phone: '+7 (900) 675-09-00',
+    email: 'konstantinopolsky@yandex.ru',
+    delivery: 'г. Москва, ул. Вечная, д. 12, кв. 56',
+    total: 1000,
+    scores: 200
+}
+
+const orders = [
+    {
+        name: 'Массажёр для лица',
+        description: 'Просто у меня есть определенный регламент по времени на выполнение моих задач.',
+        image: 'http://191n.mj.am/img/191n/3s/x4v.jpg',
+        price: '2500',
+        quantity: '2',
+    },
+    {
+        name: 'Массажёр для лица',
+        description: 'Просто у меня есть определенный регламент по времени на выполнение моих задач.',
+        image: 'http://191n.mj.am/img/191n/3s/x4g.jpg',
+        price: '1900',
+        quantity: '1',
+    },
+    {
+        name: 'Массажёр для лица',
+        description: 'Просто у меня есть определенный регламент по времени на выполнение моих задач.',
+        image: 'http://191n.mj.am/img/191n/3s/x46.jpg',
+        price: '3800',
+        quantity: '3',
+    },
+]
+
+class Email {
+    #createEmail
+    #username
+
+    constructor({username, file}) {
+        this.createBaseTemplate(file)
+        this.#username = username
+    }
+
+    createBaseTemplate(file) {
+        const mjml = fs.readFileSync(path.resolve(__dirname, file), 'utf8')
+        this.#createEmail = Handlebars.compile(mjml)
+    }
+
+    createRecentlyOrder({items, orderId, orderStatus, products}) {
+        const orderBodyMjml = fs.readFileSync(path.join(__dirname, 'order', 'successOrderBody.hbs'), 'utf8')
+        const productsMjml = fs.readFileSync(path.join(__dirname, 'order', 'orderSuccess.hbs'), 'utf8')
+        const testTemplateMjml = fs.readFileSync(path.join(__dirname, 'product', 'product.hbs'), 'utf8')
+        const linkButtonMjml = fs.readFileSync(path.join(__dirname, 'common', 'linkButton.hbs'), 'utf8')
+
+        const orderBodyTemplate = Handlebars.compile(orderBodyMjml)
+        const productsTemplate = Handlebars.compile(productsMjml)
+        const testTemplate = Handlebars.compile(testTemplateMjml)
+        const linkButtonTemplate = Handlebars.compile(linkButtonMjml)
+
+        Handlebars.registerPartial('Body', orderBodyTemplate({orderId, orderStatus}) + productsTemplate({items, ...successOrder}) + testTemplate({products}));
+        Handlebars.registerPartial('LinkButton', linkButtonTemplate({buttonText}) );
+
+        const {html} = mjml2html(this.#createEmail({name: this.#username}))
+
+        return html
+    }
+
+}
+
+const email = new Email({file: 'input.hbs', username: 'Михаил'})
+const t = email.createRecentlyOrder({items: orders, orderStatus: 'CLOSED', orderId: '№295034', products: orders})
+
+writeFile('test.html', t)
